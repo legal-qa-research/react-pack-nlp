d287e741d0a0f28953a81db0f5c140a554db4c37
Long Nguyen <anonyhost@gmail.com>
2021-05-11 23:41:46 +0700

    change host backend server

--- src/constant/server.const.js
+++ src/constant/server.const.js
@@ -1,3 +1,4 @@
-export const backendHost = 'http://222.254.30.251:5000';
+// export const backendHost = 'http://222.254.30.251:5000';
+export const backendHost = 'http://localhost:5000';
 export const spellCorrectionEndpoint = '/spell-correction';
 export const diacriticRestorationEndpoint = '/dia-res';
